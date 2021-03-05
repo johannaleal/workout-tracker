b971fdbea05a8ba9fe80766869baa1f0ab8cc280
Johanna L <johannarleal@gmail.com>
2021-03-05 15:10:32 -0500

    Added models

--- models/workout.js
+++ models/workout.js
@@ -1,14 +1,40 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  type: String,
-  name: String,
-  duration: Number,
-  weight: Number,
-  reps: Number,
-  sets: Number
-});
+const WorkoutSchema = new Schema(
+{
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]},
+    { 
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
